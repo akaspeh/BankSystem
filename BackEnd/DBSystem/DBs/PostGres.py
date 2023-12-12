556aabbf59501865a4a33e022fa1f292a029b2e0
PC <akaspeh@gmail.com>
2023-12-12 17:56:26 +0200

    Postgres init

--- BackEnd/DBSystem/DBs/PostGres.py
+++ BackEnd/DBSystem/DBs/PostGres.py
@@ -1,7 +1,6 @@
 import psycopg2
 
 class PostGresDB:
-    conn = psycopg2.connect()
     def __inti__(self, host, db, username, password):
         conn = psycopg2.connect(host=host, db=db, username=username, password=password)
 
