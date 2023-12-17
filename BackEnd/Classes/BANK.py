091b6cc84e538eea002dcfcdcb2acfa8015c9e93
PC <akaspeh@gmail.com>
2023-12-17 14:44:17 +0200

    fix bank

--- BackEnd/Classes/BANK.py
+++ BackEnd/Classes/BANK.py
@@ -13,7 +13,6 @@ class BANK:
             try:
                 cursor.execute(f"UPDATE CARDS SET balance = balance - data['amount'] WHERE card = data['cardholder'] ")
                 cursor.execute(f"UPDATE CARDS SET balance = balance + data['amount'] WHERE card = data['cardreciver'] ")
-                cursor.execute(f"UPDATE CARDS SET balance = balance + data['amount'] WHERE card = data['cardreciver'] ")
                 cursor.execute(f"INSERT INTO TRANSACTIONS (data, amount, description, user_id_sender, user_id_reciver)"
                 f"VALUES (data.today(), data['amount'], data['discription'], data['cardholder'],data['cardreciver'])")
             except Exception as e:
