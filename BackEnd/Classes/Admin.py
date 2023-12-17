1798523abcc3f160f53a3def8219e1b8d1817d14
PC <akaspeh@gmail.com>
2023-12-17 15:43:55 +0200

    fix transaction

---
+++ BackEnd/Classes/Admin.py
@@ -0,0 +1,5 @@
+
+class Admin:
+    def __init__(self, dbsystem):
+        self.__isAdmin = False
+        self.__dbsystem = dbsystem
--- BackEnd/Classes/BANK.py
+++ BackEnd/Classes/BANK.py
@@ -11,10 +11,10 @@ class BANK:
         data = request.get_json()
         with self.dbsystem.postgres.conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
             try:
-                cursor.execute(f"UPDATE CARDS SET balance = balance - data['amount'] WHERE card = data['cardholder'] ")
-                cursor.execute(f"UPDATE CARDS SET balance = balance + data['amount'] WHERE card = data['cardreciver'] ")
+                cursor.execute(f"UPDATE CARDS SET balance = balance - {data['amount']} WHERE card = {data['cardholder']} ")
+                cursor.execute(f"UPDATE CARDS SET balance = balance + {data['amount']} WHERE card = {data['cardreciver']} ")
                 cursor.execute(f"INSERT INTO TRANSACTIONS (data, amount, description, user_id_sender, user_id_reciver)"
-                f"VALUES (date.today(), data['amount'], data['description'], data['cardholder'],data['cardreciver'])")
+                f"VALUES ('{date.today()}', {data['amount']}, '{data['description']}', {data['cardholder']}, {data['cardreciver']})")
             except Exception as e:
                 logging.error(e)
     # def transactionUserToUser(self):
