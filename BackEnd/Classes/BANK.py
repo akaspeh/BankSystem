5ffa870b74ff044ceaec949978a5e0ef4628388c
PC <akaspeh@gmail.com>
2023-12-17 14:46:34 +0200

    fix bank

--- BackEnd/Classes/BANK.py
+++ BackEnd/Classes/BANK.py
@@ -14,10 +14,10 @@ class BANK:
                 cursor.execute(f"UPDATE CARDS SET balance = balance - data['amount'] WHERE card = data['cardholder'] ")
                 cursor.execute(f"UPDATE CARDS SET balance = balance + data['amount'] WHERE card = data['cardreciver'] ")
                 cursor.execute(f"INSERT INTO TRANSACTIONS (data, amount, description, user_id_sender, user_id_reciver)"
-                f"VALUES (data.today(), data['amount'], data['discription'], data['cardholder'],data['cardreciver'])")
+                f"VALUES (data.today(), data['amount'], data['description'], data['cardholder'],data['cardreciver'])")
             except Exception as e:
                 logging.error(e)
-    # def transactionUserToUser(self,card1,card2):
+    # def transactionUserToUser(self):
     #
     # def withdraw(self,card):
     #
