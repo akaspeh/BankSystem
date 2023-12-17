943633deba60eec8d61470d409b6e4482cd60ef8
Kurkuma <chopyk.mykola@lll.kpi.ua>
2023-12-17 17:49:29 +0200

    Preparing for tests

--- BackEnd/Classes/BANK.py
+++ BackEnd/Classes/BANK.py
@@ -7,16 +7,32 @@ class BANK:
     def __init__(self, dbsystem):
         self.user = User(dbsystem)
         self.dbsystem = dbsystem
-    def transactionToUser(self): #ТУТ ПЖ СДЕЛАЙ ЮНИТ ТЕСТ ДОЛБАЕБ
+    def transactionToUser(self): #ТУТ ПЖ СДЕЛАЙ ЮНИТ ТЕСТ
         data = request.get_json()
+        original_balance = self.get_balance(data['cardholder'])
+
         with self.dbsystem.postgres.conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cursor:
             try:
                 cursor.execute(f"UPDATE CARDS SET balance = balance - data['amount'] WHERE card = data['cardholder'] ")
                 cursor.execute(f"UPDATE CARDS SET balance = balance + data['amount'] WHERE card = data['cardreciver'] ")
                 cursor.execute(f"INSERT INTO TRANSACTIONS (data, amount, description, user_id_sender, user_id_reciver)"
                 f"VALUES (data.today(), data['amount'], data['description'], data['cardholder'],data['cardreciver'])")
+
+                current_balance = self.get_balance(data['cardholder'])
+                if current_balance != original_balance - data['amount']:
+                    return jsonify({'error': 'Incorrect balance after transaction'}), 404
+                else:
+                    return jsonify({'message': 'Transaction successful'}), 202
+
             except Exception as e:
                 logging.error(e)
+
+    def get_balance(self, card_number):
+        with self.dbsystem.postgres.conn.cursor() as cursor:
+            cursor.execute(f"SELECT balance FROM CARDS WHERE card = {card_number}")
+            result = cursor.fetchone()
+            return result[0] if result else None
+
     # def transactionUserToUser(self):
     #
     # def withdraw(self,card):
---
+++ BackEnd/UnitTests/BANK_UnitTests.py
@@ -0,0 +1,7 @@
+import unittest
+from unittest.mock import MagicMock
+from BackEnd.Classes.BANK import *
+
+class BANK_UnitTests:
+   def __init__(self):
+       return
